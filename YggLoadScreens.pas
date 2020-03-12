c224347155091d51372b8f769d09525a88f19a5c
yggdrasil75 <cblackburn7557@gmail.com>
2020-03-12 09:41:30 -0400

    Add files via upload

--- YggLoadScreens.pas
+++ YggLoadScreens.pas
@@ -16,11 +16,19 @@ var
 begin
 	ArtIn := TStringList.Create;
 	FindAllFiles(ArtIn,DataPath + 'Textures\Ygg\Loading', '*.jpg;*.png;*.bmp',true);
+	
 	for i := 0 to ArtIn.Count do
 	begin
-		ShellExecute(0,nil,'ImageMagick.exe','convert ' + ArtIn.Strings[i] + ' -define dd:mipmaps=1 -define dds:compression=dtx5 DDS:output_file.dds',nil,1);
-		LogMessage(1,'Converted ' + ArtIn.Strings[i] + ' to DDS');
+		ArtInTemp := ArtIn.strings[i];
+		ArtOutTemp := StringReplace(ArtInTemp, '.png', '.dds',[rfReplaceAll]);
+		ArtOutTemp := StringReplace(ArtInTemp, '.jpg', '.dds',[rfReplaceAll]);
+		ArtOutTemp := StringReplace(ArtInTemp, '.bmp', '.dds',[rfReplaceAll]);
+		ShellExecute(0,nil,'Magick.exe','convert "' + ArtIn.Strings[i] + '" -define dd:mipmaps=1 -define dds:compression=dtx5 DDS:"'+ArtOutTemp'"',nil,1);
+		LogMessage(1,'Converted ' + ArtInTemp + ' to DDS');
 	end;
 end;
 
-function 
+procedure AddLoadScreen;
+begin
+
+end;
