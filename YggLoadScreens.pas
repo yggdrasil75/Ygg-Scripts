5f168a084890053f893506bb89b99636f9c5bf34
yggdrasil75 <cblackburn7557@gmail.com>
2020-03-12 19:43:19 -0400

    Add files via upload

--- YggLoadScreens.pas
+++ YggLoadScreens.pas
@@ -17,12 +17,14 @@ begin
 	PassTime(Time);
 	Patch := SelectPatch('Ygg_Loading.esp');
 	PassFile(Patch);
-	remove(ElementByPath(Patch, 'LSCR'));
+	remove(GroupBySignature(Patch, 'LSCR'));
+	remove(GroupBySignature(Patch, 'STAT'));
 	LogMessage(0,'---Loading Is Fun---');
 	AddMessage('---Loading is fun---');
 	if not Converted then begin
 		TalkToUser := MessageDlg('There was an error in execution of imagemagick, please check that you have it installed properly including path variables', mtInformation, [mbOk], 0);
 	end;
+	AddLoadScreen;
 end;
 
 function converted: boolean;
@@ -34,40 +36,40 @@ var
 	aFolder:string;
 	Paths: tstringlist;
 begin
-{if StrToInt(ShellExecute(0,nil,'Magick.exe','convert "" -define dd:mipmaps=1 -define dds:compression=dtx5 DDS:""',nil,1)) = 2 then begin
+	{if StrToInt(ShellExecute(0,nil,'Magick.exe','convert "" -define dd:mipmaps=1 -define dds:compression=dtx5 DDS:""',nil,1)) = 2 then begin
 		result := false;
 		exit;
-		end;}
-	if not ContainsText('Magick', GetEnv('PATH')) then begin
+	end;}
+	{if not ContainsText('Magick', GetEnvironmentVariable('PATH')) then begin
 		result := false;
 		exit;
-	end;
+	end;}
 	
-	Paths := TStringlist.Create;
-	paths.DelimitedText := GetEnv('PATH');
+	{Paths := TStringlist.Create;
+	paths.DelimitedText := SysUtils.GetEnvironmentVariable('PATH');
 	for i := paths.count - 1 downto 0 do
 	begin
 		if containtsText('Magick', paths.strings[i]) then
 		MagickPath := paths.strings[i];
-	end;
+	end;}
 	
 	aFolder := DataPath + IncludeTrailingBackslash('Textures\Ygg\Loading\');
-	ArtOut := TStringList.Create;
-	ArtIn := TStringList.Create;
+	//ArtOut := TStringList.Create;
+	//ArtIn := TStringList.Create;
 	{FindAllFiles(ArtIn,DataPath + 'Textures\Ygg\Loading', '*.jpg;*.png;*.bmp',true);
 	FindAllFiles(ArtOut,DataPath + 'Textures\Ygg\Loading', '*.dds',true);}
 	LogMessage(0,'Scanning for textures in ' + aFolder);
 	ArtIn := TDirectory.GetFiles(aFolder, '*.jpg;*.png;*.bmp', soAllDirectories);
 	ArtOut := TDirectory.GetFiles(aFolder, '*.dds', soAllDirectories);
 	
-	for i := 0 to ArtIn.Count do
+	for i := 0 to Length(ArtIn) - 1 do
 	begin
 		ArtInTemp := ArtIn[i];
 		ArtOutTemp := StringReplace(ArtInTemp, '.png', '.dds',[rfReplaceAll]);
 		ArtOutTemp := StringReplace(ArtInTemp, '.jpg', '.dds',[rfReplaceAll]);
 		ArtOutTemp := StringReplace(ArtInTemp, '.bmp', '.dds',[rfReplaceAll]);
 		if ArtInTemp = ArtOutTemp then continue;
-		ShellExecute(0,nil,MagickPath+'Magick.exe','convert "' + ArtIn[i] + '" -define dd:mipmaps=1 -define dds:compression=dtx5 DDS:"'+ArtOutTemp'"',nil,1);
+		ShellExecute(0,nil,'Magick.exe','convert "' + ArtIn[i] + '" -define dd:mipmaps=1 -define dds:compression=dtx5 DDS:"'+ArtOutTemp'"',nil,1);
 		LogMessage(1,'Converted ' + ArtInTemp + ' to DDS');
 		ArtOut.Add(ArtOutTemp);
 	end;
@@ -76,18 +78,35 @@ end;
 
 procedure AddLoadScreen;
 var
-	i:integer
+	i:integer;
+	TempInt: integer;
+	CurrentEDIDAddition:string;
 	CurrentRecord,CurrentStat: IInterface;
 begin
-	for i := ArtOut.Count - 1 do begin
+	for i := Length(ArtOut) - 1 downto 0 do begin
+		tempint := length(artout[i]) - pos('\', ReverseString(ArtOut[i]))+2;
+		CurrentEDIDAddition := copy(ArtOut[i], tempint, length(artout[i]));
 		CurrentRecord := CreateRecord('LSCR');
 		CurrentStat := CreateRecord('STAT');
-		SetEditorID(CurrentStat, 'YggLoadingSTAT'+ArtOut[i]);
+		SetEditorID(CurrentStat, 'YggLoadingSTAT'+CurrentEDIDAddition);
 		
-		SetEditorID(CurrentRecord, 'YggLoadingLSCR'+ArtOut[i]);
-		SetElementEditValues(CurrentStat, 'Model\MODL', ArtOut[i]);
+		SetEditorID(CurrentRecord, 'YggLoadingLSCR'+CurrentEDIDAddition);
+		SetElementEditValues(CurrentStat, 'Model\MODL', CurrentEDIDAddition);
 		SetElementEditValues(CurrentRecord,'NNAM', name(CurrentStat));
 	end;
+end;
+
+Function ReverseString(AText: string): string;
+var
+    i,j:longint;
+begin
+  setlength(result,length(atext));
+  i:=1; j:=length(atext);
+  while (i<=j) do
+    begin
+      result[i]:=atext[j-i+1];
+      inc(i);
+    end;
 end;
 
 end.
